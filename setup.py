2cb6df6e089c5cb682efbedfb05d6ff7b3d491a0
Luis Fagundes <lhfagundes@hacklab.com.br>
2013-08-19 15:03:39 -0300

    CHANGELOG to start versioning and setup.py configuration

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 import os
 
 setup(name = 'mod-common',
-      version = '1.0',
-      description = 'MOD common libraries, used by both device and cloud',
+      version = '0.9',
+      description = 'MOD common libraries, used by both device, SDK and cloud',
       author = "Luis Fagundes",
       author_email = "lhfagundes@hacklab.com.br",
-      license = "GPL",
+      license = "GPLv3",
       packages = find_packages(),
       install_requires = ['rdflib', 'whoosh', 'pymongo'],
       classifiers = [
@@ -15,6 +15,6 @@ setup(name = 'mod-common',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
         ],
-      url = 'http://dev.portalmod.com.br/mod-common',
+      url = 'http://github.com/portalmod/mod-python',
       
 )
