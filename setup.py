0a57e0a241b3425449e314d1d23e46a9cfe1e0f8
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-04-16 10:37:05 -0300

    release v0.99.0, first release candidate for 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(name = 'mod-common',
-      version = '0.11',
+      version = '0.99.0',
       description = 'MOD common libraries, used by both device, SDK and cloud',
       author = "Luis Fagundes",
       author_email = "lhfagundes@hacklab.com.br",
