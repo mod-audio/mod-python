d76d843fccad835b36195056117f5fd0f12a4f5a
falkTX <falktx@gmail.com>
2014-11-13 00:28:12 +0000

    Consistent __init__.py

--- modcommon/__init__.py
+++ modcommon/__init__.py
@@ -1,8 +1,8 @@
-import datetime
+from datetime import datetime
 from bson.objectid import ObjectId
 
 def json_handler(obj):
-    if isinstance(obj, datetime.datetime):
+    if isinstance(obj, datetime):
         return obj.isoformat()
     if isinstance(obj, ObjectId):
         return str(obj)
