cd372e81ea11262b8a5018b8444e2f6cabb765fe
falkTX <falktx@gmail.com>
2014-11-13 03:42:22 +0000

    s/assertEquals/assertEqual/ in test_lv2.py

--- modcommon/tests/test_lv2.py
+++ modcommon/tests/test_lv2.py
@@ -21,64 +21,64 @@ class BundleTest(unittest.TestCase):
     def test_plugins_are_properly_organized(self):
         bundle = invada
 
-        self.assertEquals(len(bundle.data['plugins'].keys()), 18)
+        self.assertEqual(len(bundle.data['plugins'].keys()), 18)
 
         m = bundle.data['plugins']['http://invadarecords.com/plugins/lv2/compressor/stereo']
 
-        self.assertEquals(m['url'], 'http://invadarecords.com/plugins/lv2/compressor/stereo')
-        self.assertEquals(m['name'], 'Invada Compressor (stereo)')
-        self.assertEquals(m['maintainer']['name'], 'Invada')
-        self.assertEquals(m['maintainer']['mbox'], 'fraser@arkhostings.com')
-        self.assertEquals(m['maintainer']['homepage'],
+        self.assertEqual(m['url'], 'http://invadarecords.com/plugins/lv2/compressor/stereo')
+        self.assertEqual(m['name'], 'Invada Compressor (stereo)')
+        self.assertEqual(m['maintainer']['name'], 'Invada')
+        self.assertEqual(m['maintainer']['mbox'], 'fraser@arkhostings.com')
+        self.assertEqual(m['maintainer']['homepage'],
                           'http://www.invadarecords.com/Downloads.php?ID=00000264')
-        self.assertEquals(m['developer']['name'], 'Fraser Stuart')
-        self.assertEquals(m['developer']['mbox'], 'fraser@arkhostings.com')
-        self.assertEquals(m['developer']['homepage'],
+        self.assertEqual(m['developer']['name'], 'Fraser Stuart')
+        self.assertEqual(m['developer']['mbox'], 'fraser@arkhostings.com')
+        self.assertEqual(m['developer']['homepage'],
                           'https://launchpad.net/invada-studio')
-        self.assertEquals(m['license'], 'gpl')
+        self.assertEqual(m['license'], 'gpl')
 
-        self.assertEquals(len(m['ports']['audio']['input']), 2)
-        self.assertEquals(len(m['ports']['audio']['output']), 2)
-        self.assertEquals(len(m['ports']['control']['input']), 8)
-        self.assertEquals(len(m['ports']['control']['output']), 6)
+        self.assertEqual(len(m['ports']['audio']['input']), 2)
+        self.assertEqual(len(m['ports']['audio']['output']), 2)
+        self.assertEqual(len(m['ports']['control']['input']), 8)
+        self.assertEqual(len(m['ports']['control']['output']), 6)
 
         cs = m['ports']['control']['input']
-        self.assertEquals(cs[0]['index'], 0)
-        self.assertEquals(cs[0]['name'], 'Bypass')
-        self.assertEquals(cs[0]['symbol'], 'bypass')
-        self.assertEquals(cs[0]['default'], 0.0)
-        self.assertEquals(cs[0]['minimum'], 0)
-        self.assertEquals(cs[0]['maximum'], 1.0)
-        self.assertEquals(cs[0]['logarithmic'], False)
-        self.assertEquals(cs[1]['index'], 1)
-        self.assertEquals(cs[1]['name'], 'RMS')
-        self.assertEquals(cs[1]['symbol'], 'rms')
-        self.assertEquals(cs[1]['default'], 0.5)
-        self.assertEquals(cs[1]['minimum'], 0)
-        self.assertEquals(cs[1]['maximum'], 1.0)
-        self.assertEquals(cs[1]['logarithmic'], False)
-        self.assertEquals(cs[3]['index'], 3)
-        self.assertEquals(cs[3]['name'], 'Release')
-        self.assertEquals(cs[3]['symbol'], 'release')
-        self.assertEquals(cs[3]['default'], 0.05)
-        self.assertEquals(cs[3]['minimum'], 0.001)
-        self.assertEquals(cs[3]['maximum'], 5.0)
-        self.assertEquals(cs[3]['logarithmic'], True)
-        self.assertEquals(cs[3]['unit']['symbol'], 's')
-        self.assertEquals(cs[6]['index'], 6)
-        self.assertEquals(cs[6]['name'], 'Gain')
-        self.assertEquals(cs[6]['symbol'], 'gain')
-        self.assertEquals(cs[6]['default'], 0)
-        self.assertEquals(cs[6]['minimum'], -6)
-        self.assertEquals(cs[6]['maximum'], 36)
-        self.assertEquals(cs[6]['unit']['symbol'], 'dB')
-        self.assertEquals(cs[6]['logarithmic'], False)
+        self.assertEqual(cs[0]['index'], 0)
+        self.assertEqual(cs[0]['name'], 'Bypass')
+        self.assertEqual(cs[0]['symbol'], 'bypass')
+        self.assertEqual(cs[0]['default'], 0.0)
+        self.assertEqual(cs[0]['minimum'], 0)
+        self.assertEqual(cs[0]['maximum'], 1.0)
+        self.assertEqual(cs[0]['logarithmic'], False)
+        self.assertEqual(cs[1]['index'], 1)
+        self.assertEqual(cs[1]['name'], 'RMS')
+        self.assertEqual(cs[1]['symbol'], 'rms')
+        self.assertEqual(cs[1]['default'], 0.5)
+        self.assertEqual(cs[1]['minimum'], 0)
+        self.assertEqual(cs[1]['maximum'], 1.0)
+        self.assertEqual(cs[1]['logarithmic'], False)
+        self.assertEqual(cs[3]['index'], 3)
+        self.assertEqual(cs[3]['name'], 'Release')
+        self.assertEqual(cs[3]['symbol'], 'release')
+        self.assertEqual(cs[3]['default'], 0.05)
+        self.assertEqual(cs[3]['minimum'], 0.001)
+        self.assertEqual(cs[3]['maximum'], 5.0)
+        self.assertEqual(cs[3]['logarithmic'], True)
+        self.assertEqual(cs[3]['unit']['symbol'], 's')
+        self.assertEqual(cs[6]['index'], 6)
+        self.assertEqual(cs[6]['name'], 'Gain')
+        self.assertEqual(cs[6]['symbol'], 'gain')
+        self.assertEqual(cs[6]['default'], 0)
+        self.assertEqual(cs[6]['minimum'], -6)
+        self.assertEqual(cs[6]['maximum'], 36)
+        self.assertEqual(cs[6]['unit']['symbol'], 'dB')
+        self.assertEqual(cs[6]['logarithmic'], False)
 
     @attr(slow=1)
     def test_logarithm_port_is_properly_parsed(self):
         bundle = invada
 
-        self.assertEquals(len(bundle.data['plugins'].keys()), 18)
+        self.assertEqual(len(bundle.data['plugins'].keys()), 18)
 
         m = bundle.data['plugins']['http://invadarecords.com/plugins/lv2/delay/mono']
 
@@ -103,20 +103,20 @@ class BundleTest(unittest.TestCase):
     @attr(slow=1)
     def test_scalepoints(self):
         port = calf.data['plugins']['http://calf.sourceforge.net/plugins/Organ']['ports']['control']['input'][20]
-        self.assertEquals(len(port['scalePoints']), 36)
-        self.assertEquals(port['scalePoints'][0], {'label': 'Sin', 'value': 0.0})
-        self.assertEquals(port['scalePoints'][1], {'label': 'S0', 'value': 1.0})
-        self.assertEquals(port['scalePoints'][35], {'label': 'P:Chant', 'value': 35.0})
+        self.assertEqual(len(port['scalePoints']), 36)
+        self.assertEqual(port['scalePoints'][0], {'label': 'Sin', 'value': 0.0})
+        self.assertEqual(port['scalePoints'][1], {'label': 'S0', 'value': 1.0})
+        self.assertEqual(port['scalePoints'][35], {'label': 'P:Chant', 'value': 35.0})
 
     @attr(slow=1)
     def test_units(self):
         port = calf.data['plugins']['http://calf.sourceforge.net/plugins/Organ']['ports']['control']['input'][29]
-        self.assertEquals(port['unit']['symbol'], 'ct')
+        self.assertEqual(port['unit']['symbol'], 'ct')
 
     @attr(slow=1)
     def test_categories(self):
         inv = invada.data['plugins']
-        self.assertEquals(inv['http://invadarecords.com/plugins/lv2/compressor/stereo']['category'],
+        self.assertEqual(inv['http://invadarecords.com/plugins/lv2/compressor/stereo']['category'],
                           ['Dynamics', 'Compressor'])
 
     @attr(slow=1)
@@ -138,10 +138,10 @@ class BundleTest(unittest.TestCase):
     @attr(slow=1)
     def test_midi_ports(self):
         plugin = invada.data['plugins']['http://invadarecords.com/plugins/lv2/compressor/mono']
-        self.assertEquals(len(plugin['ports']['midi']['input']), 2)
-        self.assertEquals(plugin['ports']['midi']['input'][0]['symbol'], 'event_in')
-        self.assertEquals(plugin['ports']['midi']['input'][0]['name'], 'Event Midi')
-        self.assertEquals(plugin['ports']['midi']['input'][1]['symbol'], 'atom_in')
+        self.assertEqual(len(plugin['ports']['midi']['input']), 2)
+        self.assertEqual(plugin['ports']['midi']['input'][0]['symbol'], 'event_in')
+        self.assertEqual(plugin['ports']['midi']['input'][0]['name'], 'Event Midi')
+        self.assertEqual(plugin['ports']['midi']['input'][1]['symbol'], 'atom_in')
 
     @attr(slow=1)
     def test_bundle_id(self):
@@ -154,7 +154,7 @@ class BundleTest(unittest.TestCase):
 
         try:
             shutil.copytree(os.path.join(ROOT, 'invada.lv2'), new_inv)
-            self.assertEquals(Bundle(new_inv).data['_id'], inv_id)
+            self.assertEqual(Bundle(new_inv).data['_id'], inv_id)
             open(os.path.join(new_inv, 'delme.now'), 'w')
             self.assertNotEquals(Bundle(new_inv).data['_id'], inv_id)
         finally:
@@ -172,7 +172,7 @@ class BundleTest(unittest.TestCase):
         try:
             shutil.copytree(os.path.join(ROOT, 'invada.lv2'), new_inv)
             new_comp = Bundle(new_inv).data['plugins']['http://invadarecords.com/plugins/lv2/compressor/stereo']
-            self.assertEquals(new_comp['_id'], comp['_id'])
+            self.assertEqual(new_comp['_id'], comp['_id'])
         finally:
             shutil.rmtree(new_inv)
 
@@ -183,21 +183,21 @@ class BundleTest(unittest.TestCase):
 
         from bson.objectid import ObjectId
 
-        self.assertEquals(str(ObjectId(comp['_id'])), str(comp['_id']))
-        self.assertEquals(str(ObjectId(inv['_id'])), str(inv['_id']))
+        self.assertEqual(str(ObjectId(comp['_id'])), str(comp['_id']))
+        self.assertEqual(str(ObjectId(inv['_id'])), str(inv['_id']))
 
     @attr(slow=1)
     def test_plugin_package_data(self):
         comp = invada.data['plugins']['http://invadarecords.com/plugins/lv2/compressor/stereo']
         inv = invada.data
 
-        self.assertEquals(comp['package_id'], inv['_id'])
-        self.assertEquals(comp['package'], 'invada.lv2')
+        self.assertEqual(comp['package_id'], inv['_id'])
+        self.assertEqual(comp['package'], 'invada.lv2')
 
     @attr(slow=1)
     def test_plugin_url(self):
         comp = invada.data['plugins']['http://invadarecords.com/plugins/lv2/compressor/stereo']
-        self.assertEquals(comp['url'], 'http://invadarecords.com/plugins/lv2/compressor/stereo')
+        self.assertEqual(comp['url'], 'http://invadarecords.com/plugins/lv2/compressor/stereo')
 
     @attr(slow=1)
     def test_units_path(self):
@@ -207,7 +207,7 @@ class BundleTest(unittest.TestCase):
         try:
             os.chdir('..')
             inv = Bundle(os.path.join(ROOT, 'invada.lv2'), units_file='/tmp/units.ttl')
-            self.assertEquals(invada.data['_id'], inv.data['_id'])
+            self.assertEqual(invada.data['_id'], inv.data['_id'])
         finally:
             os.remove('/tmp/units.ttl')
 
@@ -218,14 +218,14 @@ class BundleTest(unittest.TestCase):
         delay = invada.data['plugins']['http://invadarecords.com/plugins/lv2/delay/mono']
         filter = invada.data['plugins']['http://invadarecords.com/plugins/lv2/filter/lpf/mono']
 
-        self.assertEquals(erreverb['version'], '0.0')
-        self.assertEquals(erreverb['stability'], 'experimental')
-        self.assertEquals(compressor['version'], '0.1')
-        self.assertEquals(compressor['stability'], 'testing')
-        self.assertEquals(delay['version'], '1.1')
-        self.assertEquals(delay['stability'], 'unstable')
-        self.assertEquals(filter['version'], '0.2')
-        self.assertEquals(filter['stability'], 'stable')
+        self.assertEqual(erreverb['version'], '0.0')
+        self.assertEqual(erreverb['stability'], 'experimental')
+        self.assertEqual(compressor['version'], '0.1')
+        self.assertEqual(compressor['stability'], 'testing')
+        self.assertEqual(delay['version'], '1.1')
+        self.assertEqual(delay['stability'], 'unstable')
+        self.assertEqual(filter['version'], '0.2')
+        self.assertEqual(filter['stability'], 'stable')
 
 class BundlePackageTest(unittest.TestCase):
     @attr(slow=1)
@@ -240,7 +240,7 @@ class BundlePackageTest(unittest.TestCase):
             subprocess.Popen(['tar', 'zxf', 'plugin.tgz']).wait()
             os.chdir(cur_dir)
             bundle = Bundle(os.path.join(tmp_dir, 'invada.lv2'))
-            self.assertEquals(bundle.data['_id'], invada.data['_id'])
+            self.assertEqual(bundle.data['_id'], invada.data['_id'])
         finally:
             os.chdir(cur_dir)
             shutil.rmtree(tmp_dir)
